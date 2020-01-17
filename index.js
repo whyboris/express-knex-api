6ebd1dc7e9b0c996c1cb56f3094ca0af9fe70f24
whyboris <yboris@yahoo.com>
2020-01-17 16:07:15 -0500

    jsdoc & remaining params

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ var knex = require('knex')({
   }
 });
 
+/**
+ * Route to search for businesses
+ * @route {GET} /businesses
+ * @param {string} [name]    - name of business,    substring search
+ * @param {string} [address] - address of business, substring search
+ * @param {string} [city]    - city of business,    exact match
+ * @param {string} [state]   - city of business,    exact match
+ */
 app.get('/businesses', function (req, res) {
   console.log(req.query);
 
@@ -21,6 +29,14 @@ app.get('/businesses', function (req, res) {
 
   if (req.query.name) {
     query.where('name','like','%' + req.query.name + '%');
+  }
+
+  if (req.query.address) {
+    query.where('address','like','%' + req.query.address + '%');
+  }
+
+  if (req.query.city) {
+    query.where('city', req.query.address);
   }
 
   if (req.query.state) {
